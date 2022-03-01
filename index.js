0fd16b04a19bba2a5ca4ee671b021eaebccdf281
Marcus Kjell <marcus.kjell@gmail.com>
2022-03-01 20:47:19 +0100

    Project created

---
+++ index.js
@@ -0,0 +1,26 @@
+// module.exports = {
+//   // parser: "@typescript-eslint/parser",
+//   // plugins: ["@typescript-eslint"],
+//   extends: [
+//     "eslint:recommended",
+//     "plugin:@typescript-eslint/recommended",
+//     "next/core-web-vitals",
+//     "plugin:prettier/recommended",
+//   ],
+// };
+
+module.exports = {
+  extends: ["eslint:recommended", "next/core-web-vitals", "plugin:prettier/recommended"],
+  rules: {
+    "@next/next/no-html-link-for-pages": "off",
+    "react/jsx-key": "off",
+  },
+  overrides: [
+    {
+      files: ["**/*.ts?(x)"],
+      parser: "@typescript-eslint/parser",
+      plugins: ["@typescript-eslint"],
+      extends: ["plugin:@typescript-eslint/recommended"],
+    },
+  ],
+};
